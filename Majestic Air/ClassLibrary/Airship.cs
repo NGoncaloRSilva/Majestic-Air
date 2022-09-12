ac991269ed3b08a29dedfaa23bc97302cc71725c
Nuno Gonçalo Silva <ngoncalorsilva@gmail.com>
2022-09-12 15:42:01 +0100

    Class Creation

---
+++ Majestic Air/ClassLibrary/Airship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Airship
+    {
+        public int Id { get; set; }
+        public string AirshipName { get; set; }
+        public Model model { get; set; }
+    }
+}
---
+++ Majestic Air/ClassLibrary/Trip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public DateTime Day { get; set; }
+        public DateTime Hour { get; set; }
+        public Airship AirshipName { get; set; }
+        public double Price1stClass { get; set; }
+        public double PriceBusiness { get; set; }
+        public double PricePremiumEconomy { get; set; }
+        public double PriceEconomy { get; set; }
+        //public int IdOrigin { get; set; }
+        //public int IdDestination { get; set; }
+        public Location Origin { get; set; }
+        public Location Destination { get; set; }
+    }
+}
